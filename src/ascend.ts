31b13abd61e272a9ffab22818664b95dfac3e2b9
CheeseyPickle <86745595+CheeseyPickle@users.noreply.github.com>
2021-08-17 10:07:28 -0500

    Ascend with correct options

--- src/ascend.ts
+++ src/ascend.ts
@@ -10,11 +10,11 @@ visitUrl('afterlife.php?action=pearlygates');
 
 // Pilsners
 visitUrl('afterlife.php?action=buydeli&whichitem=5046');
-// Statuette
-visitUrl('afterlife.php?action=buyarmory&whichitem=5037');
+// Belt
+visitUrl('afterlife.php?action=buyarmory&whichitem=5042');
 
-// Pastamancer, Blender, Male, CS
+// Pastamancer, Wallaby, Male, CS
 visitUrl(
-  'afterlife.php?action=ascend&confirmascend=1&whichsign=8&gender=1&whichclass=3&whichpath=25&asctype=2&nopetok=1&noskillsok=1&pwd',
+  'afterlife.php?action=ascend&confirmascend=1&whichsign=2&gender=1&whichclass=3&whichpath=25&asctype=2&nopetok=1&noskillsok=1&pwd',
   true
 );
