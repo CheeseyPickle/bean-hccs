14c1a64ddca3e439a0ad0b3c5e6a582a8d3bb38f
CheeseyPickle <86745595+CheeseyPickle@users.noreply.github.com>
2023-02-18 21:45:11 -0600

    Configure trainset before ascension if you can

--- src/pre.ts
+++ src/pre.ts
@@ -1,5 +1,12 @@
-import { abort, availableAmount, cliExecute, pvpAttacksLeft, use } from "kolmafia";
-import { $item, get } from "libram";
+import {
+  abort,
+  availableAmount,
+  cliExecute,
+  getWorkshed,
+  pvpAttacksLeft,
+  use,
+} from "kolmafia";
+import { $item, get, TrainSet } from "libram";
 import { ensureItem } from "./lib";
 
 const season = get("currentPVPSeason");
@@ -9,7 +16,7 @@ const PVP_STANCE: { [key: string]: string } = {
   pirate: "Smellin' Like a Stinkin' Rose",
   glitch: "Installation Wizard",
   numeric: "A Nice Cold One",
-  ice: "A Nice Cold One"
+  ice: "A Nice Cold One",
 };
 
 const getTarget = () => {
@@ -31,6 +38,23 @@ if (pvpAttacksLeft() > 0) {
 cliExecute("refresh inventory");
 if (availableAmount($item`bran muffin`) < 1) {
   abort("No bran muffin");
+}
+
+// TODO: Check ice house for remaindered skeleton
+if (
+  getWorkshed() == $item`model train set` &&
+  get("trainsetPosition") - get("lastTrainsetConfiguration") >= 40
+) {
+  TrainSet.setConfiguration([
+    TrainSet.Station.GAIN_MEAT,
+    TrainSet.Station.BRAIN_SILO,
+    TrainSet.Station.VIEWING_PLATFORM,
+    TrainSet.Station.CANDY_FACTORY,
+    TrainSet.Station.GRAIN_SILO,
+    TrainSet.Station.TRACKSIDE_DINER,
+    TrainSet.Station.ORE_HOPPER,
+    TrainSet.Station.COAL_HOPPER
+  ]);
 }
 
 !noError && abort();
